64b20d3a95301ada809efa9b0eeee2e23303e562
RoseMontoya <98787013+RoseMontoya@users.noreply.github.com>
2024-08-08 15:45:39 -0700

    mostly done with cart models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .db import db
 from .user import User
 from .db import environment, SCHEMA
+from .cart import Cart, CartItem
--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,9 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from sqlalchemy.orm import relationship #type: ignore
 
+print('Database in USERS',db)
 
 class User(db.Model, UserMixin):
     __tablename__ = "users"
@@ -13,6 +15,8 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+
+    cart = relationship('Cart', back_populates='user', cascade='all, delete-orphan')
 
     @property
     def password(self):
