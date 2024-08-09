0569f1120ee9ec7abbeec8636dcadd4f2ac4f2f8
Sarah Jiang <sarahjiang@Sarahs-MacBook-Air.local>
2024-08-09 09:44:05 -0700

    ready to render

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 function LandingPage(){
     return (
         <>
-            <h2>There's a Person Behind Every Piece</h2>
+            <h2>There&apos;s a Person Behind Every Piece</h2>
             <p>Fall in love with original finds from standout small shops around the world.</p>
             <button >Discover more</button>
         </>
