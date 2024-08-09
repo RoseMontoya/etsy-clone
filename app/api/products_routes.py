0b0549cb7353537705436d2f9540509c110e6ac7
RoseMontoya <98787013+RoseMontoya@users.noreply.github.com>
2024-08-09 16:53:48 -0700

    finished backend route and reducer for get all reviews

--- app/api/products_routes.py
+++ app/api/products_routes.py
@@ -1,12 +1,18 @@
 from flask import Blueprint
 from ..models.product import Product
+from ..models.review import Review
 
 products_routes = Blueprint("products", __name__)
 
+# Get all reviews for a product
+@products_routes.route('/<int:productId>/reviews')
+def product_reviews(productId):
+    reviews = Review.query.filter(Review.product_id == productId).all()
+    return [ review.to_dict() for review in reviews ]
 
 
 # Get product by product id
-@products_routes.route("/<productId>")
+@products_routes.route("/<int:productId>")
 def product_by_id(productId):
     product = Product.query.filter(Product.id == productId).one()
     return product.to_dict()
