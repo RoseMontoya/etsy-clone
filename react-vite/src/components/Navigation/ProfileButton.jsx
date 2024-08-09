8bb1dbacbfe3e8de68e459325d41a966245a4f4a
RoseMontoya <98787013+RoseMontoya@users.noreply.github.com>
2024-08-09 09:26:26 -0700

    fixed merge difference

--- react-vite/src/components/Navigation/ProfileButton.jsx
+++ react-vite/src/components/Navigation/ProfileButton.jsx
@@ -55,7 +55,8 @@ function ProfileButton() {
           itemText="Sign in"
           onItemClick={closeMenu}
           modalComponent={<LoginFormModal />}
-        />
+          />
+
         )}
       </button>
       {showMenu && (
