1494b7c6d6009b37a1b1917480bb76e715b81f17
RoseMontoya <98787013+RoseMontoya@users.noreply.github.com>
2024-08-12 15:16:51 -0700

    pulled changes

---
+++ react-vite/src/components/MainProfile_Fav/MainProfile_Fav 2.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { thunkAllProducts } from "../../redux/product";
+// import { Link } from "react-router-dom";
+
+import "./MainProfile.css";
+function HomePage() {
+  const dispatch = useDispatch();
+  let product = useSelector((state) => {
+    console.log(state);
+    return state.product.allProducts;
+  });
+  console.log("Product from MainProfile", product);
+  useEffect(() => {
+    dispatch(thunkAllProducts());
+  }, [dispatch]);
+  if (!product) return null;
+  return <>Welcome back,</>;
+}
+export default HomePage;
