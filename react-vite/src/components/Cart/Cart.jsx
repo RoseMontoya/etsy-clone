838d92042ec2086ca1707e4aca23e6234a034171
shanda <brucewang147862@gmail.com>
2024-08-13 21:06:24 -0700

    Stil working on cart

--- react-vite/src/components/Cart/Cart.jsx
+++ react-vite/src/components/Cart/Cart.jsx
@@ -1,37 +1,50 @@
-import "./Cart.css"
-import { useEffect } from "react"
-import { useSelector, useDispatch } from "react-redux"
-import { Link, useNavigate } from "react-router-dom"
+import "./Cart.css";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import { getAllCartItems } from "../../redux/cart";
 
 function Cart() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const user = useSelector((state) => state.session.user)
-    const cartObj = useSelector((state) => state.cart?.cart)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.session.user);
+  const cartObj = useSelector((state) => state.cart?.cartItems);
 
-    const cartArr = cartObj ? Object.values(cartObj) : []
-    // console.log("FRONT END ========>", cartObj)
+  const cartArr = cartObj ? Object.values(cartObj) : [];
+  // console.log("FRONT END ========>", cartObj)
 
-    useEffect(() => {
-        if(!cartObj){
-            dispatch(getAllCartItems())
-        }
-    }, [dispatch])
+  useEffect(() => {
+    if (!cartObj) {
+      dispatch(getAllCartItems());
+    }
+  }, [dispatch]);
 
-    const cartLength = Object.values()
+  // const cartLength = Object.values()
 
-    return (
-        <>
-            <div>Cart Page</div>
+  return (
+    <>
+      <div>Cart Page</div>
 
-            {/* <div>
-                <span>{cartArr.length >= 1 && (
-                    <div>{cartArr.length === 1 ? cartArr.length + " item in your cart" : cartArr.length + " items in your cart"}</div>
-                )}</span>
-            </div> */}
+      <div>
+        <span>
+          {cartArr.length > 0 ? (
+            <div>
+              {cartArr.length === 1
+                ? `${cartArr.length} item in your cart`
+                : `${cartArr.length} items in your cart`}
+            </div>
+          ) : (
+            <div>
+              <p>Your cart is empty</p>
+              <Link to="/products">
+                Discover something unique to fill it up
+              </Link>
+            </div>
+          )}
+        </span>
+      </div>
 
-            {/* <div>
+      {/* <div>
                 {cartArr && cartArr?.length === 0 ? (
                     <>
                         <p>Your cart is empty</p>
@@ -43,8 +56,8 @@ function Cart() {
                     `${cartArr?.length} items in your cart`
                 )}
             </div> */}
-        </>
-    );
-};
+    </>
+  );
+}
 
 export default Cart;
