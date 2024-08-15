34bd1c275f05969462938c6df5ac897d3def200f
shanda <brucewang147862@gmail.com>
2024-08-15 14:36:53 -0700

    some css on product manage page and form page

--- react-vite/src/components/ProductForm/CreateProduct.jsx
+++ react-vite/src/components/ProductForm/CreateProduct.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { addProduct, addProductImage, productByUserId } from "../../redux/product";
+import {
+  addProduct,
+  addProductImage,
+  productByUserId,
+} from "../../redux/product";
 import "./ProductForm.css";
 
 function NewProductForm() {
@@ -36,46 +40,57 @@ function NewProductForm() {
     const result = await dispatch(addProduct(new_product));
 
     const productId = result.id;
-    let imageArray = [{
-      product_id: productId,
-      url: previewImageUrl,
-      preview: true,}];
-    if (image1Url) imageArray.push({
+    let imageArray = [
+      {
+        product_id: productId,
+        url: previewImageUrl,
+        preview: true,
+      },
+    ];
+    if (image1Url)
+      imageArray.push({
         product_id: productId,
         url: image1Url,
         preview: false,
-    })
-    if (image2Url) imageArray.push({
-      product_id: productId,
-      url: image2Url,
-      preview: false,
-    })
-    if (image3Url) imageArray.push({
-      product_id: productId,
-      url: image3Url,
-      preview: false,
-    })
-    if (image4Url) imageArray.push({
-      product_id: productId,
-      url: image4Url,
-      preview: false,
-    })
-    if (image5Url) imageArray.push({
-      product_id: productId,
-      url: image5Url,
-      preview: false,
-    })
-    
-    await Promise.all(imageArray.map(image => dispatch(addProductImage(image, user.id))));
+      });
+    if (image2Url)
+      imageArray.push({
+        product_id: productId,
+        url: image2Url,
+        preview: false,
+      });
+    if (image3Url)
+      imageArray.push({
+        product_id: productId,
+        url: image3Url,
+        preview: false,
+      });
+    if (image4Url)
+      imageArray.push({
+        product_id: productId,
+        url: image4Url,
+        preview: false,
+      });
+    if (image5Url)
+      imageArray.push({
+        product_id: productId,
+        url: image5Url,
+        preview: false,
+      });
+
+    await Promise.all(
+      imageArray.map((image) => dispatch(addProductImage(image, user.id)))
+    );
     dispatch(productByUserId());
     navigate(`/products/${productId}`);
   };
   return (
     <form onSubmit={handleSubmit} className="product_form">
       <div>
-        <label><h3>Title</h3></label>
-        <p>Include keywords that buyers would use to search for this item.
-        </p>
+        <label>
+          <h3>Title</h3>
+        </label>
+        <p>Include keywords that buyers would use to search for this item.</p>
         <input
           type="text"
           value={title}
@@ -84,8 +99,12 @@ function NewProductForm() {
         />
       </div>
       <div>
-        <label><h3>Description</h3></label>
-        <p>Tell the world all about your item and why they’ll love it. Buyers will only see the first few lines unless they expand the description.
+        <label>
+          <h3>Description</h3>
+        </label>
+        <p>
+          Tell the world all about your item and why they’ll love it. Buyers
+          will only see the first few lines unless they expand the description.
         </p>
         <textarea
           value={description}
@@ -94,8 +113,13 @@ function NewProductForm() {
         ></textarea>
       </div>
       <div>
-        <label><h3>Inventory</h3></label>
-        <p>Keep your product availability up-to-date to ensure customers know when your item is in stock.</p>
+        <label>
+          <h3>Inventory</h3>
+        </label>
+        <p>
+          Keep your product availability up-to-date to ensure customers know
+          when your item is in stock.
+        </p>
         <input
           type="number"
           value={inventory}
@@ -105,8 +129,13 @@ function NewProductForm() {
         />
       </div>
       <div>
-        <label><h3>Price</h3></label>
-        <p>Competitive pricing can help your listing stand out and rank higher in search results.</p>
+        <label>
+          <h3>Price</h3>
+        </label>
+        <p>
+          Competitive pricing can help your listing stand out and rank higher in
+          search results.
+        </p>
         <input
           type="number"
           value={price}
@@ -116,8 +145,13 @@ function NewProductForm() {
         />
       </div>
       <div>
-        <label><h3>Category</h3></label>
-        <p>Categorize your product accurately to help customers find it more easily.</p>
+        <label>
+          <h3>Category</h3>
+        </label>
+        <p>
+          Categorize your product accurately to help customers find it more
+          easily.
+        </p>
         <select
           name="category_id"
           value={categoryId}
@@ -133,7 +167,9 @@ function NewProductForm() {
         </select>
       </div>
       <div>
-        <label><h3>Preview Image URL</h3></label>
+        <label>
+          <h3>Preview Image URL</h3>
+        </label>
         <p>Submit at least one photo to publish your product.</p>
         <input
           type="text"
@@ -141,6 +177,14 @@ function NewProductForm() {
           onChange={(e) => setPreviewImageUrl(e.target.value)}
           required
         />
+
+        {previewImageUrl ? (
+          <img
+            className="previewImagesize"
+            src={previewImageUrl}
+            alt="Preview if Image is valid"
+          />
+        ) : null}
       </div>
       <div>
         <label>Image URL:</label>
@@ -149,6 +193,14 @@ function NewProductForm() {
           value={image1Url}
           onChange={(e) => setImage1Url(e.target.value)}
         />
+
+        {image1Url ? (
+          <img
+            className="previewImagesize"
+            src={image1Url}
+            alt="Preview if Image is valid"
+          />
+        ) : null}
       </div>
       <div>
         <label>Image URL:</label>
@@ -157,6 +209,14 @@ function NewProductForm() {
           value={image2Url}
           onChange={(e) => setImage2Url(e.target.value)}
         />
+
+        {image2Url ? (
+          <img
+            className="previewImagesize"
+            src={image2Url}
+            alt="Preview if Image is valid"
+          />
+        ) : null}
       </div>
       <div>
         <label>Image URL:</label>
@@ -165,6 +225,14 @@ function NewProductForm() {
           value={image3Url}
           onChange={(e) => setImage3Url(e.target.value)}
         />
+
+        {image3Url ? (
+          <img
+            className="previewImagesize"
+            src={image3Url}
+            alt="Preview if Image is valid"
+          />
+        ) : null}
       </div>
       <div>
         <label>Image URL:</label>
@@ -173,6 +241,14 @@ function NewProductForm() {
           value={image4Url}
           onChange={(e) => setImage4Url(e.target.value)}
         />
+
+        {image4Url ? (
+          <img
+            className="previewImagesize"
+            src={image4Url}
+            alt="Preview if Image is valid"
+          />
+        ) : null}
       </div>
       <div>
         <label>Image URL:</label>
@@ -181,6 +257,14 @@ function NewProductForm() {
           value={image5Url}
           onChange={(e) => setImage5Url(e.target.value)}
         />
+
+        {image5Url ? (
+          <img
+            className="previewImagesize"
+            src={image5Url}
+            alt="Preview if Image is valid"
+          />
+        ) : null}
       </div>
       <button type="submit">Publish Your Product</button>
     </form>
