e9fc26134b866ea957a6e9339196a2a1026709ff
Sarah Jiang <sarahjiang@Sarahs-MacBook-Air.local>
2024-08-15 14:41:06 -0700

    merge from css

--- react-vite/src/components/ProductList/ProductList.jsx
+++ react-vite/src/components/ProductList/ProductList.jsx
@@ -14,7 +14,7 @@ function ProductList() {
   const productsObj = useSelector((state) => state.products?.allProducts);
   const user = useSelector((state) => state.session.user);
   const products = productsObj ? Object.values(productsObj) : [];
-  const favoritesObj = useSelector(state => state.favorites?.[user.id])
+  const favoritesObj = useSelector(state => state.favorites?.[user?.id])
   const favProducts = favoritesObj? Object.values(favoritesObj).map(fav => fav.product.id): [];
 
 
@@ -24,7 +24,7 @@ function ProductList() {
       dispatch(thunkAllProducts());
     }
     if (!favoritesObj && user) {
-      dispatch(favoritesByUserId(user.id))
+      dispatch(favoritesByUserId(user?.id))
     }
   }, [dispatch, productsObj, favoritesObj, user]);
 
