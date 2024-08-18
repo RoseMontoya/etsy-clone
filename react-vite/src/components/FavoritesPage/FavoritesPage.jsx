8f9122db01843b2b642c4c2fdf41732e1cb45b84
RoseMontoya <98787013+RoseMontoya@users.noreply.github.com>
2024-08-17 21:00:08 -0700

    think im finally done with css for today. update favs page

--- react-vite/src/components/FavoritesPage/FavoritesPage.jsx
+++ react-vite/src/components/FavoritesPage/FavoritesPage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { favoritesByUserId } from "../../redux/favorite";
 import { addToCart, getAllCartItems } from "../../redux/cart";
 import { useNavigate } from "react-router-dom";
-import {Heart} from "../SubComponents";
+import { Heart } from "../SubComponents";
 import { TiStarFullOutline } from "react-icons/ti";
 import "./FavoritesPage.css";
 import { thunkAllProducts } from "../../redux/product";
@@ -14,17 +14,17 @@ function FavoritesPage() {
   const navigate = useNavigate();
   const user = useSelector((state) => state.session.user);
   const favorites = useSelector((state) => state.favorites?.[user?.id]);
-  const allProducts = useSelector((state) => state.products.allProducts)
-  const favoritesArray = favorites? Object.values(favorites): [];
+  const allProducts = useSelector((state) => state.products.allProducts);
+  const favoritesArray = favorites ? Object.values(favorites) : [];
 
   useEffect(() => {
     if (!favorites) {
       dispatch(favoritesByUserId(user.id));
     }
     if (!allProducts) {
-      dispatch(thunkAllProducts())
+      dispatch(thunkAllProducts());
     }
-  }, [dispatch, favorites, allProducts,user.id]);
+  }, [dispatch, favorites, allProducts, user.id]);
 
   if (!favorites) return <p>Loading...</p>;
 
@@ -43,44 +43,54 @@ function FavoritesPage() {
     });
   };
 
-
   return (
     <main>
+    <div className="product_manage_header">
       <div className="favorite_profile">
         <img src={user.profile_url} alt={user.username} />
-        <p>{user.first_name}&apos;s Favorites</p>
+        <h2>{user.first_name}&apos;s Favorites</h2>
+      </div>
       </div>
       <div className="grid_container">
-        {favoritesArray.length && allProducts? (
+        {favoritesArray.length && allProducts ? (
           favoritesArray.map((favorite) => {
-            return allProducts[favorite.product_id]?
-            (
-            <div key={favorite.id} className="grid-item" id="fav-grid-item">
-            <Heart initial={true} productId={favorite.product_id}/>
-              <Link key={favorite.id} to={`/products/${favorite.product_id}`}>
-                <div className="image_container">
-                  <img
-                    src={allProducts[favorite.product_id].preview_image}
-                    style={{ width: '100%' }}
-                    alt={allProducts[favorite.product_id].title}
-                  />
-                </div>
-                <div className="grid-item-detail">
-                <p className="title">{allProducts[favorite.product_id].title}</p>
-                <p>${allProducts[favorite.product_id].price.toFixed(2)}</p>
-                <p>
-                  {allProducts[favorite.product_id].seller.seller_rating}
-                  <TiStarFullOutline />({allProducts[favorite.product_id].seller.review_count})
-                </p>
-
-                </div>
-
-              </Link>
-              <button onClick={() => handleAddToCart(allProducts[favorite.product_id])}>
-              + Add to cart
-            </button>
-            </div>
-          ): null})
+            return allProducts[favorite.product_id] ? (
+              <div key={favorite.id} className="grid-item" id="fav-grid-item">
+                <Heart initial={true} productId={favorite.product_id} />
+                <Link key={favorite.id} to={`/products/${favorite.product_id}`}>
+                  <div className="image_container">
+                    <img
+                      src={allProducts[favorite.product_id].preview_image}
+                      style={{ width: "100%" }}
+                      alt={allProducts[favorite.product_id].title}
+                    />
+                  </div>
+                  <div className="grid-item-detail">
+                    <div className='upper-fav-details'>
+                      <p className="title">
+                        {allProducts[favorite.product_id].title}
+                      </p>
+                      <div className="rating">
+                        <p className="bold" style={{fontSize: '14px'}}>{allProducts[favorite.product_id].seller.seller_rating}</p>
+                        <TiStarFullOutline />
+                        <span className="count">
+                          ({allProducts[favorite.product_id].seller.review_count})
+                        </span>
+                      </div>
+                    </div>
+                    <p className="bold" style={{fontSize: '20px'}}>${allProducts[favorite.product_id].price.toFixed(2)}</p>
+                  </div>
+                </Link>
+                <button
+                  onClick={() =>
+                    handleAddToCart(allProducts[favorite.product_id])
+                  }
+                >
+                  + Add to cart
+                </button>
+              </div>
+            ) : null;
+          })
         ) : (
           <p>
             Start favoriting items to compare, shop, and keep track of things
