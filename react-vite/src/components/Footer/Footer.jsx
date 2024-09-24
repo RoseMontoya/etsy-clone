f22360275eaf354a82d4d9beb9a3d7655de4b8dc
o0saraho0 <sarah.jiang.1229@gmail.com>
2024-09-24 09:24:25 -0700

    footer image added

--- react-vite/src/components/Footer/Footer.jsx
+++ react-vite/src/components/Footer/Footer.jsx
@@ -10,47 +10,76 @@ function Footer() {
       </div>
       <div className="team">
         <div className="team-member">
-          <p>Laiba Afzal</p>
-          <div className="team-link">
-            <a href="https://github.com/lai-baa">
-              <FaGithub />
+          <div>
+            <a href="https://laiba-afzal-portfolio.netlify.app/">
+              <img src="/images/Laiba.webp" alt="Laiba" />
             </a>
-            <a href="https://www.linkedin.com/in/laibaafzal">
-              <FaLinkedin />
+          </div>
+          <div className="member-info">
+            <p>Laiba Afzal</p>
+            <div className="team-link">
+              <a href="https://github.com/lai-baa">
+                <FaGithub />
+              </a>
+              <a href="https://www.linkedin.com/in/laibaafzal">
+                <FaLinkedin />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="team-member">
+          <div>
+            <a href="https://rose-montoya.netlify.app/">
+              <img src="/images/Rose.png" alt="Rose" />
             </a>
+          </div>
+          <div>
+            <p>Rose Montoya</p>
+            <div className="team-link">
+              <a href="https://github.com/RoseMontoya">
+                <FaGithub />
+              </a>
+              <a href="https://www.linkedin.com/in/rose-montoya/">
+                <FaLinkedin />
+              </a>
+            </div>
           </div>
         </div>
         <div className="team-member">
-          <p>Rose Montoya</p>
-          <div className="team-link">
-            <a href="https://github.com/RoseMontoya">
-              <FaGithub />
+          <div>
+            <a href="https://portfolio-sarah-jiang.netlify.app/">
+              <img src="/images/Sarah.JPG" alt="Sarah" />
             </a>
-            <a href="https://www.linkedin.com/in/rose-montoya/">
-              <FaLinkedin />
-            </a>
+          </div>
+          <div>
+            <p>Sarah Jiang</p>
+            <div className="team-link">
+              <a href="https://github.com/o0saraho0">
+                <FaGithub />
+              </a>
+              <a href="https://www.linkedin.com/in/sarah-yue-jiang/">
+                <FaLinkedin />
+              </a>
+            </div>
           </div>
         </div>
         <div className="team-member">
-          <p>Sarah Jiang</p>
-          <div className="team-link">
-            <a href="https://github.com/o0saraho0">
-              <FaGithub />
-            </a>
-            <a href="https://www.linkedin.com/in/sarah-yue-jiang/">
-              <FaLinkedin />
+          <div>
+            <a href="https://portifolio-rpiy.onrender.com/">
+              <img src="/images/Shanda.png" alt="Shanda" />
             </a>
           </div>
-        </div>
-        <div className="team-member">
-          <p>Shanda Wang</p>
-          <div className="team-link">
-            <a href="https://github.com/shandawang1005">
-              <FaGithub />
-            </a>
-            <a href="https://www.linkedin.com/in/shanda-wang/">
-              <FaLinkedin />
-            </a>
+          <div>
+            <p>Shanda Wang</p>
+            <div className="team-link">
+              <a href="https://github.com/shandawang1005">
+                <FaGithub />
+              </a>
+              <a href="https://www.linkedin.com/in/shanda-wang/">
+                <FaLinkedin />
+              </a>
+            </div>
           </div>
         </div>
       </div>
